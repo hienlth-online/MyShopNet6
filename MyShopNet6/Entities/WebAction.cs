32db49fe8104d0874079911ba013ea530b005c03
Hien Luong <hyhien@gmail.com>
2022-09-19 08:40:02 +0700

    Add project files.

---
+++ MyShopNet6/Entities/WebAction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyShopNet6.Entities
+{
+    public partial class WebAction
+    {
+        public WebAction()
+        {
+            RoleActions = new HashSet<RoleAction>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Method { get; set; } = null!;
+        public string Description { get; set; } = null!;
+
+        public virtual ICollection<RoleAction> RoleActions { get; set; }
+    }
+}
---
+++ MyShopNet6/Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using MyShopNet6.Entities;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<MyShopContext>(option => option.UseSqlServer(builder.Configuration.GetConnectionString("MyShop")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
